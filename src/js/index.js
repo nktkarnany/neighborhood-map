f26aa0b525d6cceb5cdd45c2923f204daaa324cc
Ankit Karnany <ankit@greylabs.org>
2017-04-05 21:01:25 +0530

    code review comments resolved

--- src/js/index.js
+++ src/js/index.js
@@ -4,21 +4,13 @@ var model = {
         lng: -73.99802
     },
     places: []
-}
+};
 
 var map;
-var markers = [];
 var infoWindows = [];
 
 // function to initialize the map within the map div
 function initMap() {
-
-    // function to check if maps api failed then an alert shows up
-    setTimeout(function () {
-        if (!window.google || !window.google.maps) {
-            alert("Not able to load google maps...Please try again later!!");
-        }
-    }, 5000);
 
     // loading the google maps
     map = new google.maps.Map(document.getElementById('map'), {
@@ -29,6 +21,11 @@ function initMap() {
     // function called to get the list of places for the current location
     getCurrentLocation();
 
+}
+
+// error handling for the google maps api
+function googleError() {
+    alert("Sorry! Not able to load google maps. Please try again later.");
 }
 
 function getCurrentLocation() {
@@ -67,9 +64,6 @@ function getPlaces() {
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-        results.forEach(function (result, index) {
-            results[index].index = index;
-        });
         model.places = results;
         initModel(); //called if got list of all the places
     } else {
@@ -91,16 +85,16 @@ function initModel() {
             this.toggle(!this.toggle()); // on clicking on the toggle menu button animates the side bar menu out of the screen
         },
 
-        showDetail: function (i) { // called for mouse over event on a place in the list view
-            var index = viewModel.places()[i].index;
-            markers[index].setAnimation(google.maps.Animation.BOUNCE);
-            infoWindows[index].open(map, markers[index]);
-        },
-
-        hideDetail: function (i) { // called for mouse out event on a place in the list view
-            var index = viewModel.places()[i].index;
-            markers[index].setAnimation(null);
-            infoWindows[index].close(map, markers[index]);
+        placeClicked: function(place) { // called when place in the list view is clicked
+            var marker = place.marker;
+            var infowindow = place.infoWindow;
+            if (infowindow.isOpen()) {
+                infowindow.close(map, marker);
+                marker.setAnimation(null);
+            } else {
+                infowindow.open(map, marker);
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
         },
 
         query: ko.observable(''),
@@ -109,13 +103,8 @@ function initModel() {
             var search = viewModel.query().toLowerCase();
             return ko.utils.arrayFilter(model.places, function (place, index) {
                 var match = place.name.toLowerCase().indexOf(search) >= 0;
-                if (match) {
-                    markers[index].setMap(map);
-                    return true;
-                } else {
-                    markers[index].setMap(null);
-                    return false;
-                }
+                place.marker.setVisible(match);
+                return match;
             });
         })
     };
@@ -129,7 +118,6 @@ function renderAllMarkers() {
         return (map !== null && typeof map !== "undefined");
     }
     model.places.forEach(function (place, index) {
-
         // generating markers for each place in places array model
         var marker = new google.maps.Marker({
             position: {
@@ -141,14 +129,12 @@ function renderAllMarkers() {
 
         // creating and saving info windows into an array
         var infowindow = new google.maps.InfoWindow({
-            content: place.name
+            content: place.vicinity
         });
         infowindow.addListener('closeclick', function () {
             infowindow.close(map, marker);
             marker.setAnimation(null);
         });
-
-        infoWindows.push(infowindow);
 
         // adding a click listener on the marker to display the info window
         marker.addListener('click', function () {
@@ -160,18 +146,21 @@ function renderAllMarkers() {
                 marker.setAnimation(google.maps.Animation.BOUNCE);
             }
         });
-        markers.push(marker);
+        model.places[index].marker = marker;
+        model.places[index].infoWindow = infowindow;
     });
-    updateInfoWindows(infoWindows);
+    updateInfoWindows();
 }
 
-var updateInfoWindows = function (infos) {
+var updateInfoWindows = function () {
 
-    infos.forEach(function (info, index) {
+    model.places.forEach(function (place, index) {
+
+        var infoWindow = model.places[index].infoWindow;
 
         // getting the data for info windows from wikipedia api
         $.ajax({
-            url: 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + info.getContent(),
+            url: 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + place.name,
             type: 'get',
             dataType: "jsonp",
 
@@ -179,14 +168,14 @@ var updateInfoWindows = function (infos) {
                 var link = response[3][0];
 
                 if (link) {
-                    infoWindows[index].setContent("<a href='" + link + "'>" + link + "</a>");
+                    infoWindow.setContent("<a href='" + link + "' target=\"blank\">" + link + "</a>");
                 } else {
-                    infoWindows[index].setContent("No Wiki!!");
+                    infoWindow.setContent("No Wiki!!");
                 }
 
             },
             error: function () {
-                infowindow[index].setContent("Sorry!! Lost connection");
+                infoWindow.setContent("Sorry!! Lost connection");
             }
         });
 
